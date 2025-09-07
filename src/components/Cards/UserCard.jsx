41602af7ca0c28b11f5f326b6c9451adaaa7a174
Oleksandr <gale249@gmail.com>
2025-09-07 16:25:06 +0300

    .

--- src/components/Cards/UserCard.jsx
+++ src/components/Cards/UserCard.jsx
@@ -58,7 +58,7 @@ export default function UserCard({
               width: "70px",
               height: "70px",
             }}
-            alt={noPhoto}
+            alt={name}
             children={noPhoto}
             src={photo}
           />
