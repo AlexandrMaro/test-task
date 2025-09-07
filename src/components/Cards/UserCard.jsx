d57d2e6fb6dd3d977df0554c673c27fe45d3e8b6
Oleksandr <gale249@gmail.com>
2025-09-07 16:23:42 +0300

    add Avatar's children

--- src/components/Cards/UserCard.jsx
+++ src/components/Cards/UserCard.jsx
@@ -59,7 +59,7 @@ export default function UserCard({
               height: "70px",
             }}
             alt={noPhoto}
-            imgProps={{ onerror: () => setImgSrc(noPhoto) }}
+            children={noPhoto}
             src={photo}
           />
         ) : (
